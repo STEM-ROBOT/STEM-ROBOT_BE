3e2ce0f310e9a6fb81abcfa309cb12e847f4ce4b
longnhatlekk <135359437+longnhatlekk@users.noreply.github.com>
2024-10-29 16:07:10 +0700

    fix error

--- STEM-ROBOT.Common/Rsp/TournamentRep.cs
+++ STEM-ROBOT.Common/Rsp/TournamentRep.cs
@@ -31,7 +31,13 @@ namespace STEM_ROBOT.Common.Rsp
     {
         public string imageCompetition { get; set; }
     }
-
+    public class TournamentInforRsp
+    {
+        public string? Name { get; set; }
+        public string? Location { get; set; }
+        public string? Image { get; set; }
+        public int? NumberTeam { get; set; }
+    }
     public class TournamentModerator
     {
         public int Id { get; set; }
