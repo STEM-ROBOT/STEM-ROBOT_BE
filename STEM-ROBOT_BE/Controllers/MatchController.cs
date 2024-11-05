e1ef397292acfa3337685324eefecdb4d4cf0e5f
HoangChiDuong <duonghoangasd@gmail.com>
2024-11-05 09:42:06 +0700

    update range match 0.

--- STEM-ROBOT_BE/Controllers/MatchController.cs
+++ STEM-ROBOT_BE/Controllers/MatchController.cs
@@ -39,7 +39,7 @@ namespace STEM_ROBOT.Web.Controllers
         [HttpGet("match-config-time")]
         public async Task<IActionResult> getRoundTableGame(int CompetitionID)
         {
-            bool isFormatTable =true;
+            bool isFormatTable = true;
             var res = await _matchSvc.getListRound(CompetitionID, isFormatTable);
             if (!res.Success) throw new Exception("Check again");
             return Ok(res);
@@ -80,6 +80,17 @@ namespace STEM_ROBOT.Web.Controllers
         public IActionResult AddStage(MatchReq request)
         {
             var res = _matchSvc.AddMatch(request);
+            if (!res.Success)
+            {
+                res.SetMessage(res.Message);
+            }
+            return Ok(res);
+        }
+
+        [HttpPut("")]
+        public async Task<IActionResult> ConFigTimeMtch(int competition, MatchConfigReq request)
+        {
+            var res = await _matchSvc.conFigTimeMtch(competition, request);
             if (!res.Success)
             {
                 res.SetMessage(res.Message);
