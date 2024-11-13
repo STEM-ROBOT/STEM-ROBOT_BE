1d5abd09f7f01f6a034d1560f075a47d2c4e0d21
HoangChiDuong <duonghoangasd@gmail.com>
2024-11-13 13:41:18 +0700

    Api teamregister 0.1

---
+++ STEM-ROBOT_BE/Controllers/TeamRegisterController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using STEM_ROBOT.BLL.Svc;
+using STEM_ROBOT.Common.Req;
+
+namespace STEM_ROBOT.Web.Controllers
+{
+    [Route("api/teams-register")]
+    [ApiController]
+
+    public class TeamRegisterSvc : ControllerBase
+    {
+        private readonly TeamRegisterSvc _teamRegisterSvc;
+        public TeamRegisterSvc(TeamRegisterSvc teamRegisterSvc)
+        {
+            _teamRegisterSvc = teamRegisterSvc;
+        }
+        [HttpGet("")]
+        public async Task<IActionResult> GetListTeamMatch(int competitionId, List<TeamMatchConfigCompetition> teamsMatchs)
+        {
+            
+            return Ok();
+        }
+        [HttpPost("")]
+        public async Task<IActionResult> RegisterTeamCompetion(int competitionId, List<TeamMatchConfigCompetition> teamsMatchs)
+        {
+
+            return Ok();
+        }
+    }
+
+}
