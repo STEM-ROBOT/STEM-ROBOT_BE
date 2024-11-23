b78ba637780a02e42058c6455b311761d6dd6ed4
LDT1403 <thanhldse160337@fpt.edu.vn>
2024-11-23 13:29:52 +0700

    fix timein and timout

--- STEM-ROBOT.BLL/Svc/ScheduleSvc.cs
+++ STEM-ROBOT.BLL/Svc/ScheduleSvc.cs
@@ -550,8 +550,8 @@ namespace STEM_ROBOT.BLL.Svc
                     {
                         breakHaftTime = matchData.BreakTimeHaft.ToString(),
                         durationHaft = matchData.TimeOfHaft.Value.ToString(),
-                        endTime = matchData.TimeIn.Value.ToString(),
-                        startTime = matchData.TimeOut.Value.ToString(),
+                        endTime = matchData.TimeOut.Value.ToString(),
+                        startTime = matchData.TimeIn.Value.ToString(),
                         startDate = matchData.StartDate.Value.ToString(),
                         haftMatch = matchData.MatchHalves.Select(h => new ScheduleMatchHaftRsp
                         {
