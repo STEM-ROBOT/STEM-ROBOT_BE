f126f54eb7399718b2473cd9bd2aedce55ea2661
HoangChiDuong <duonghoangasd@gmail.com>
2024-11-25 15:12:57 +0700

    update CORS

--- STEM-ROBOT_BE/Program.cs
+++ STEM-ROBOT_BE/Program.cs
@@ -31,7 +31,7 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("myAppCors", policy =>
     {
-        policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "https://stem-robot-system.vercel.app")
+        policy.WithOrigins("http://localhost:5173", "http://localhost:3000", "http://157.66.27.69:5173")
                  .AllowAnyHeader()
                  .AllowAnyMethod()
                     .AllowCredentials();
